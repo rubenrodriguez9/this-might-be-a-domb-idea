7e26d11aa9d1337c5a0e392fb02d1d156e9e6882
Ruben Rodriguez <ruben.rodriguez@codeimmersives.com>
2020-08-03 19:18:19 -0400

    finished

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
-1
+const paragraph = document.querySelector('p');
+paragraph.style.color = 'lightblue';
+
+const header = document.querySelector('h1');
+header.style.fontSize = '5em';
+
+paragraph.innerHTML = 'Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.'
+
+const item13 = document.querySelector('#item-13');
+
+item13.style.opacity = '0.5';
+
+const item3 = document.querySelector('#item-3');
+
+item3.innerHTML = 'I say, "Hi!'
+
+let dino = document.querySelector('img');
+
+dino.src = 'http://www.tioxic.com/wp-content/uploads/trex_4.jpg';
+dino.style.height = '300px';
+
+
+const brooklyn = document.querySelector('#brooklyn');
+
+brooklyn.src = 'https://i.insider.com/4f9ff0f4ecad04d414000007?width=600&format=jpeg&auto=webp';
+
+brooklyn.style.height = '300px'
+
+
+const item16 = document.querySelector('#item-16');
+item16.innerHTML = `Won't get fooled again`
+
